4ddaa709c7fa3e96ba945b50c19b1370fdd63e08
Wouter <wouterbuthker@live.nl>
2018-10-25 22:32:28 +0200

    Viewing json part 1

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,42 +1,64 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
 import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 import {Observable} from "rxjs";
 
+
+
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
+
+
 export class HomePage {
 
-  constructor(private http: HttpClient) {
-
-        this.http.get('/assets/Json/Almelo.json').subscribe(
-            data => {
-                console.log(data);
-            }, (err: HttpErrorResponse) => {
-                console.log(err.message);
+    locations : Observable<object[]>;
+    testArray : number[];
+    testString : string;
+    testObj : object[];
+    constructor(private http: HttpClient) {
+        this.testString = "test";
+        this.locations = this.loadDataFromJson();
+        this.testArray = [1,2,3,4];
+        this.testObj = [
+            {
+                "properties": {
+                    "name" : "fail"
+                }
+            },
+            {
+                "properties" : {
+                    "name" : "fail"
+                }
             }
-        )
-      // load the data and print it in the console
-      // this.getJSON().subscribe(data => {
-      //     console.log(data)
-      // });
+        ];
+    }
 
-
-  }
-
-  // public getJSON(): Observable<any> {
-  //     return this.http.get("assets/Json/Almelo.json"); //TODO: add compatibility for other places
-  // }
 
     ionViewDidLoad() {
-        this.loadDataFromJson();
+
+        //
     }
 
 
     loadDataFromJson() {
+        // load the json data from a file
+        let locations;
 
+        this.http.get('/assets/Json/Almelo.json').subscribe(
+            data => {
+                locations = data['features'];
+
+
+                console.log("succes");
+                return locations;
+            }, (err: HttpErrorResponse) => {
+                console.log(err.message);
+            }
+        );
+        console.log("nope");
+        return locations;
     }
+
 
 }
