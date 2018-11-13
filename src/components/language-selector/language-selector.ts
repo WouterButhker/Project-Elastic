ad25fc7f31ae70c3bdb2c45a4270a3f0e937eda1
WouterButhker <wouterbuthker@live.nl>
2018-11-13 20:46:44 +0100

    refactored and improved language selector

--- src/components/language-selector/language-selector.ts
+++ src/components/language-selector/language-selector.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ViewController } from "ionic-angular";
 
+
 /**
  * Generated class for the LanguageSelectorComponent component.
  *
@@ -8,32 +9,74 @@ import { ViewController } from "ionic-angular";
  * Components.
  */
 @Component({
-  selector: 'language-selector',
-  templateUrl: 'language-selector.html'
+    selector: 'language-selector',
+    templateUrl: 'language-selector.html'
 })
 export class LanguageSelectorComponent {
 
+    selectedCity: string;
 
-  nativeLanguage: string;
-  nativeFlag: string;
-  selectedCity: string;
-  selectedLanguage: string;
+    selectedLanguage: string;
+    cities: object[];
+    imgBasePath: string;
 
-  constructor(public viewCtrl: ViewController) {
-    this.nativeLanguage = 'Dutch';
-    this.nativeFlag = 'netherlands.png';
-      this.selectedLanguage = 'Dutch';
-      this.selectedCity = 'Almelo';
-  }
+    constructor(public viewCtrl: ViewController) {
 
-  cancelLanguage() {
-      this.viewCtrl.dismiss();
-  }
+        this.imgBasePath = 'assets/Pictures/Flags/';
+        this.selectedLanguage = 'Dutch';
 
-  saveLanguage(city, language) {
-    console.log(this.selectedCity);
-    console.log(this.selectedLanguage);
+        this.cities = [
+            {
+                name: "Almelo",
+                viewName: "Almelo",
+                image: this.imgBasePath + "netherlands.png",
+                language: "Dutch",
+                viewLanguage: "Nederlands"
+            },
+            {
+                name: "Nordhorn",
+                viewName: "Nordhorn",
+                image: this.imgBasePath + "germany.png",
+                language: "German",
+                viewLanguage: "Deutsch"
+            },
+            {
+                name: "Zelow",
+                viewName: "Zelów",
+                image: this.imgBasePath + "poland.png",
+                language: "Polish",
+                viewLanguage: "Język polski"
+            },
+            {
+                name: "Valasske",
+                viewName: "Valašské Klobouky",
+                image: this.imgBasePath + "czech-republic.png",
+                language: "Czech",
+                viewLanguage: "Český jazyk"
+            }];
 
-  }
+        this.selectedCity = "Almelo";
+    }
 
+    cancelLanguage() {
+        this.viewCtrl.dismiss();
+    }
+
+    saveLanguage() {
+        console.log(this.selectedCity);
+        console.log(this.selectedLanguage);
+
+    }
+
+    checkCity(cityName) {
+        if (cityName == "Almelo") {
+            return 0;
+        } else if (cityName == "Nordhorn") {
+            return 1;
+        } else if (cityName == "Zelow") {
+            return 2;
+        } else if (cityName == "Valasske") {
+            return 3;
+        }
+    }
 }
