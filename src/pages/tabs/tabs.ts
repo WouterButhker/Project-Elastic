d252a3ffd5de480221345b96e8fd24c36a55ed42
WouterButhker <wouterbuthker@live.nl>
2019-01-07 21:04:06 +0100

    fix package

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { MapPage } from '../map/map';
 import { HomePage } from '../home/home';
 import {DataManagerProvider} from "../../providers/data-manager/data-manager";
+import {NavParams} from "ionic-angular";
 
 @Component({
   templateUrl: 'tabs.html'
@@ -13,7 +14,7 @@ export class TabsPage {
   tab2Root = MapPage;
 
 
-  constructor(private dataManager: DataManagerProvider) {
+  constructor(private dataManager: DataManagerProvider, private navParams: NavParams) {
 
   }
 }
