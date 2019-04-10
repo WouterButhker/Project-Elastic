2945a4f7faee736be98b7ca41da5e58653f83269
WouterButhker <wouterbuthker@live.nl>
2019-04-10 20:07:37 +0200

    Updated contact page

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,16 +1,20 @@
 import { Component, ViewChild } from '@angular/core';
 import { NavController, List } from 'ionic-angular';
 
+
 @Component({
     selector: 'page-contact',
     templateUrl: 'contact.html'
 })
 export class ContactPage {
     @ViewChild(List) list: List;
+    contributors;
 
     constructor(public navCtrl: NavController) {
-    }
 
+        // this.contributors = this.loadDataFromJson();
+        console.log(this.contributors);
+    }
 
 
 }
