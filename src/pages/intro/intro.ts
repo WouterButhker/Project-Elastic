c002291f9e7315615c9f3790a8646979bd2239cb
WouterButhker <wouterbuthker@live.nl>
2019-05-28 14:59:42 +0200

    Changed intropage

--- src/pages/intro/intro.ts
+++ src/pages/intro/intro.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {IonicPage, NavController, NavParams, PopoverController} from 'ionic-angular';
+import {IonicPage, NavController, NavParams, Platform, PopoverController} from 'ionic-angular';
 import {TabsPage} from "../tabs/tabs";
 import {DataManagerProvider} from "../../providers/data-manager/data-manager";
 import {TranslateService} from "@ngx-translate/core";
@@ -15,44 +15,52 @@ import {GoogleAnalytics} from "@ionic-native/google-analytics";
 
 @IonicPage()
 @Component({
-  selector: 'page-intro',
-  templateUrl: 'intro.html',
+    selector: 'page-intro',
+    templateUrl: 'intro.html',
 })
 export class IntroPage {
 
-  constructor(public navCtrl: NavController,
-              private dataManager: DataManagerProvider,
-              private translate: TranslateService,
-              private popoverCtrl: PopoverController,
-              private ga: GoogleAnalytics) {
+    constructor(public navCtrl: NavController,
+                private dataManager: DataManagerProvider,
+                private translate: TranslateService,
+                private popoverCtrl: PopoverController,
+                private ga: GoogleAnalytics,
+                private platform: Platform) {
 
-      this.checkUserLanguage();
-
-      this.setupTracking();
-  }
+        this.checkUserLanguage();
 
 
-  public start(city) {
-      this.dataManager.setCity(city);
-      this.navCtrl.setRoot(TabsPage, {});
-  }
+    }
 
-  private checkUserLanguage() {
-      this.translate.setDefaultLang("English"); // sets default language to English
-      if (navigator.language.startsWith("en")) {
-          this.dataManager.setLanuage("English");
-      } else if (navigator.language.startsWith("nl")) {
-          this.dataManager.setLanuage("Dutch");
-      } else if (navigator.language.startsWith("pl")) {
-          this.dataManager.setLanuage("Polish");
-      } else if (navigator.language.startsWith("cs")) {
-        this.dataManager.setLanuage("Czech");
-      } else if (navigator.language.startsWith('de'))  {
-          this.dataManager.setLanuage("German")
-      } else {
-          this.dataManager.setLanuage("English")
-      }
-  }
+    ionViewDidLoad() {
+        this.gaStartTracker();
+        // this.dataManager.gaTrackView("Intro")
+    }
+
+
+    public start(city) {
+        this.dataManager.setCity(city);
+        this.navCtrl.setRoot(TabsPage, {}).catch((reason: any)=> {
+            console.log(reason)
+        })
+    }
+
+    private checkUserLanguage() {
+        this.translate.setDefaultLang("English"); // sets default language to English
+        if (navigator.language.startsWith("en")) {
+            this.dataManager.setLanuage("English");
+        } else if (navigator.language.startsWith("nl")) {
+            this.dataManager.setLanuage("Dutch");
+        } else if (navigator.language.startsWith("pl")) {
+            this.dataManager.setLanuage("Polish");
+        } else if (navigator.language.startsWith("cs")) {
+            this.dataManager.setLanuage("Czech");
+        } else if (navigator.language.startsWith('de'))  {
+            this.dataManager.setLanuage("German")
+        } else {
+            this.dataManager.setLanuage("English")
+        }
+    }
 
     private openLanguageSelector(myEvent) {
         let popover = this.popoverCtrl.create(
@@ -62,15 +70,30 @@ export class IntroPage {
 
         popover.present( {
             ev: myEvent
+        }).catch((reason: any)=> {
+            console.log(reason)
         });
 
     }
 
-    private setupTracking() {
-      this.ga.startTrackerWithId('UA-126931548-1').then(() => {
-          console.log("GA started");
-          this.ga.trackView("Intro");
-      })
-          .catch(e => console.log("Error starting GA", e));
+    private gaStartTracker(){
+        this.platform.ready().then(() => {
+
+            this.ga.debugMode();
+            // this.ga.enableUncaughtExceptionReporting(true);
+
+            this.ga.startTrackerWithId('UA-126931548-2')
+                .then(() => {
+                    console.log("GA started");
+                    this.ga.trackView("Intro").catch((e) => {
+                        console.log("nope", e)
+                    })
+                })
+                .catch(e => {
+                    console.log("Error starting GA", e)
+                });
+
+        });
+
     }
 }
